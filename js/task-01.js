a674d2c4c0657073f4bcf5c39d029a2b14d85fb7
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-29 14:35:39 +0300

    Зад 1 выведены в консоль названия категорий

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,14 @@
 const itemEl = document.querySelectorAll(".item");
-console.log(itemEl);
+console.log("Number of categories: ", itemEl.length);
+const categorieTitleEl = document.querySelectorAll(".item h2");
+console.log(categorieTitleEl);
+const categorieTitle0TextEl = categorieTitleEl[0].textContent;
+console.log("Category: ", categorieTitle0TextEl);
+
+console.log("Elements: ");
+const categorieTitle1TextEl = categorieTitleEl[1].textContent;
+console.log("Category: ", categorieTitle1TextEl);
+console.log("Elements: ");
+const categorieTitle2TextEl = categorieTitleEl[2].textContent;
+console.log("Category: ", categorieTitle2TextEl);
+console.log("Elements: ");
