af898f1c195e8fbaf3f72ab35935546fd21d44fc
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-30 19:23:56 +0300

    Задание 1 изменение решения через детей и потомков

--- js/task-01.js
+++ js/task-01.js
@@ -3,17 +3,20 @@ console.log("Number of categories: ", itemEl.length);
 
 const categorieTitleEl = document.querySelectorAll(".item h2");
 
-const categorieTitle0TextEl = categorieTitleEl[0].textContent;
-console.log("Category: ", categorieTitle0TextEl);
-const cat1El = document.querySelectorAll(".js-item-animals li");
-console.log("Elements: ", cat1El.length);
+const categorieTitleTextEl0 = categorieTitleEl[0].textContent;
+console.log("Category: ", categorieTitleTextEl0);
+const categorieAnimalsChieldEl = itemEl[0].childNodes;
+const childrenAnimalsChieldEL = categorieAnimalsChieldEl[3].children;
+console.log("Elements: ", childrenAnimalsChieldEL.length);
 
 const categorieTitle1TextEl = categorieTitleEl[1].textContent;
 console.log("Category: ", categorieTitle1TextEl);
-const cat2El = document.querySelectorAll(".js-item-products li");
-console.log("Elements: ", cat2El.length);
+const categorieProductChieldEl = itemEl[1].childNodes;
+const childrenProductChieldEL = categorieProductChieldEl[3].children;
+console.log("Elements: ", childrenProductChieldEL.length);
 
 const categorieTitle2TextEl = categorieTitleEl[2].textContent;
 console.log("Category: ", categorieTitle2TextEl);
-const cat3El = document.querySelectorAll(".js-item-technologies li");
-console.log("Elements: ", cat3El.length);
+const categorieTechnologChieldEl = itemEl[2].childNodes;
+const childrenTechnologChieldEL = categorieTechnologChieldEl[3].children;
+console.log("Elements: ", childrenTechnologChieldEL.length);
