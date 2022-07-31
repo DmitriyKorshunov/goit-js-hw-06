7b9f491ce518c54f5253a2f222eeef63c9d85562
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-31 21:10:55 +0300

    Зад 1 изменение названия переменных

--- js/task-01.js
+++ js/task-01.js
@@ -3,20 +3,20 @@ console.log("Number of categories: ", itemEl.length);
 
 const categorieTitleEl = document.querySelectorAll(".item h2");
 
-const categorieTitleTextEl0 = categorieTitleEl[0].textContent;
-console.log("Category: ", categorieTitleTextEl0);
+const categorieTitleAnimalsEl = categorieTitleEl[0].textContent;
+console.log("Category: ", categorieTitleAnimalsEl);
 const categorieAnimalsChieldEl = itemEl[0].childNodes;
 const childrenAnimalsChieldEL = categorieAnimalsChieldEl[3].children;
 console.log("Elements: ", childrenAnimalsChieldEL.length);
 
-const categorieTitle1TextEl = categorieTitleEl[1].textContent;
-console.log("Category: ", categorieTitle1TextEl);
+const categorieTitleProductEl = categorieTitleEl[1].textContent;
+console.log("Category: ", categorieTitleProductEl);
 const categorieProductChieldEl = itemEl[1].childNodes;
 const childrenProductChieldEL = categorieProductChieldEl[3].children;
 console.log("Elements: ", childrenProductChieldEL.length);
 
-const categorieTitle2TextEl = categorieTitleEl[2].textContent;
-console.log("Category: ", categorieTitle2TextEl);
+const categorieTitleTechnologEl = categorieTitleEl[2].textContent;
+console.log("Category: ", categorieTitleTechnologEl);
 const categorieTechnologChieldEl = itemEl[2].childNodes;
 const childrenTechnologChieldEL = categorieTechnologChieldEl[3].children;
 console.log("Elements: ", childrenTechnologChieldEL.length);
