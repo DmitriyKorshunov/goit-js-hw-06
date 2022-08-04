911e7495dc09a95c161ead2eeb2639abcebb2060
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-04 12:09:28 +0300

    Задание 1 изменение решения через forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,7 @@
-const itemEl = document.querySelectorAll(".item");
-console.log("Number of categories: ", itemEl.length);
+const categoriesList = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesList.length}`);
 
-const categorieTitleEl = document.querySelectorAll(".item h2");
-
-const categorieTitleAnimalsEl = categorieTitleEl[0].textContent;
-console.log("Category: ", categorieTitleAnimalsEl);
-const categorieAnimalsChieldEl = itemEl[0].childNodes;
-const childrenAnimalsChieldEL = categorieAnimalsChieldEl[3].children;
-console.log("Elements: ", childrenAnimalsChieldEL.length);
-
-const categorieTitleProductEl = categorieTitleEl[1].textContent;
-console.log("Category: ", categorieTitleProductEl);
-const categorieProductChieldEl = itemEl[1].childNodes;
-const childrenProductChieldEL = categorieProductChieldEl[3].children;
-console.log("Elements: ", childrenProductChieldEL.length);
-
-const categorieTitleTechnologEl = categorieTitleEl[2].textContent;
-console.log("Category: ", categorieTitleTechnologEl);
-const categorieTechnologChieldEl = itemEl[2].childNodes;
-const childrenTechnologChieldEL = categorieTechnologChieldEl[3].children;
-console.log("Elements: ", childrenTechnologChieldEL.length);
+categoriesList.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
