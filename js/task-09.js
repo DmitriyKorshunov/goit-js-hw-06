7fe32d766d01e7d5d0b8ad373b2bb1d43b33df46
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-05 13:12:48 +0300

    Задание 9 решено

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const bodyEl = document.querySelector("body");
+const btnChangeColorEl = document.querySelector(".change-color");
+const spanColorTextEL = document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+btnChangeColorEl.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanColorTextEL.textContent = color;
+});
