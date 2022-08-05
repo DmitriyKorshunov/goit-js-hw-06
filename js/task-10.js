2fa40285a6b90344e2d956942d44fb6284ee5892
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-05 15:21:05 +0300

    Задание 10 создание переменных

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const btnCreateAction = document.querySelector("[data-create]");
+console.log(btnCreateAction);
+const btnDestroyaction = document.querySelector("[data-destroy]");
+console.log(btnDestroyaction);
+const divBoxesEL = document.querySelector("#boxes");
+console.log(divBoxesEL);
+let amount = document.getElementsByTagName("input").value;
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+function createBoxes(amount) {
+  console.log(amount);
+
+  console.log(divEl);
 }
+const divEl = document.createElement("div");
+divEl.width = 30;
+divEl.height = 30;
