c27b79a4828e30d12a5e9185d48c7362fe8451bd
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-06 21:44:11 +0300

    Задание 6 исправлено

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,8 @@
 const textInput = document.querySelector("#validation-input");
-console.log(textInput);
-console.log(textInput.getAttribute("data-length"));
 
 textInput.addEventListener("blur", (event) => {
-  console.log(event);
-  console.log(event.target.value.length);
-  return event.target.value.length == textInput.getAttribute("data-length")
-    ? textInput.classList.add("valid")
-    : textInput.classList.add("invalid");
+  Number(event.target.value.length) !==
+  Number(textInput.getAttribute("data-length"))
+    ? textInput.classList.add("invalid")
+    : textInput.classList.replace("invalid", "valid");
 });
