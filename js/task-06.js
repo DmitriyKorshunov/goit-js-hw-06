311022e3200279030732f94d20223491f0f69c02
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-06 16:34:25 +0300

    Задание 6 замена if на тернарный оператор, решения нет

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,11 @@
 const textInput = document.querySelector("#validation-input");
+console.log(textInput);
+console.log(textInput.getAttribute("data-length"));
 
 textInput.addEventListener("blur", (event) => {
-  if (event.target.value.length == textInput.getAttribute("data-length")) {
-    textInput.classList.add("valid");
-  } else {
-    textInput.classList.replace("valid", "invalid");
-  }
+  console.log(event);
+  console.log(event.target.value.length);
+  return event.target.value.length == textInput.getAttribute("data-length")
+    ? textInput.classList.add("valid")
+    : textInput.classList.add("invalid");
 });
